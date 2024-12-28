56ddf288f7291059a0447476d97d0f6e983090e7
NishantChahar <nishantchahar12102002@gmail.com>
2024-12-28 22:47:29 +0530

    im

--- src/componenets/login-signup/login.jsx
+++ src/componenets/login-signup/login.jsx
@@ -5,10 +5,12 @@ import password_icon from '../Assets/password.png'
 import person_icon from '../Assets/person.png'
 
 const Login = () => {
-  const [action,setaction] = useState("Login");
+  const [action,setaction] = useState("Signup");
   const [name,setName] = useState();
   const [email,setEmail] = useState();
   const [password,setPassword] = useState();
+  const [phoneNumber,setPhoneNumber] = useState();
+  const [address,setAddress] = useState();
 
   const namechangehandle=(e)=>{
     setName(e.target.value)
@@ -21,14 +23,29 @@ const Login = () => {
   const passwordchangehandle=(e)=>{
     setPassword(e.target.value)
   }
+  const phonenochangehandle=(e)=>{
+    setPassword(e.target.value)
+  }
+  const addresschangehandle=(e)=>{
+    setPassword(e.target.value)
+  }
+
   
-  const loginhandle=()=>{
+  const loginhandle=(e)=>{
+    console.log(e)
+    const data1 ={email,password}
+    console.log(data1)
     setaction("Login")
+    setEmail(null)
+    setPassword(null)
   }
 
   const Signuphandle=()=>{
-    const data ={name,email,password}
+    const data2 ={name,email,password,phoneNumber,address}
+    console.log(data2)
     setaction("Signup")
+    setPassword(null)
+    setEmail(null)
   }
   
   return (
@@ -40,15 +57,23 @@ const Login = () => {
         <div className='inputs'>
             <div className={action==='Login'?"hidden-user":"input"}>
                 <img src={person_icon} alt=''/>
-                <input type='text' placeholder='Name' onChange={namechangehandle}/>
+                <input type='text' placeholder='Name' onChange={namechangehandle} value={name}/>
             </div>
             <div className='input'>
                 <img src={email_icon} alt=''/>
-                <input type='email' placeholder='Email' onChange={emailchangehandle}/>
+                <input type='email' placeholder='Email' onChange={emailchangehandle}value={email}/>
             </div>
             <div className='input'>
                 <img src={password_icon} alt=''/>
-                <input type='password'placeholder='Password' onChange={passwordchangehandle}/>
+                <input type='password'placeholder='Password' onChange={passwordchangehandle}value={password}/>
+            </div>
+            <div className={action==='Login'?"hidden-user":"input"}>
+                <img src={password_icon} alt=''/>
+                <input type='text'placeholder='Phone No' onChange={phonenochangehandle}value={phoneNumber}/>
+            </div>
+            <div className={action==='Login'?"hidden-user":"input"}>
+                <img src={password_icon} alt=''/>
+                <input type='text'placeholder='Address' onChange={addresschangehandle}value={address}/>
             </div>
         </div>
         <div className={action==="Login"?"forgot-password":"hidden-user"}>lost password <span>Click Here</span></div>
